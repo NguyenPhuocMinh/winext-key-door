1ca9aa8ea1226e97401dcd95208a03b3f53d4de3
Minh Nguyen <xiuquay55@gmail.com>
2022-05-16 00:38:43 +0700

    update code

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const generateToken = (secret) => {};
+const fs = require('fs');
+const winext = require('winext');
+const jwt = winext.require('jsonwebtoken');
+
+const generateToken = (data = {}, algorithm = 'SHA256', expiresIn = 3600) => {
+  const privateKey = fs.readFileSync('jwtRS256.key');
+
+  const token = jwt.sign(data, privateKey, { algorithm: algorithm, expiresIn: expiresIn });
+
+  return token;
+};
 
 module.exports = generateToken;
